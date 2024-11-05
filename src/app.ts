78992a782b6501f5e1dea5c173f8563f13d99acc
Clément <poudree.clement@gmail.com>
2024-11-05 18:35:04 +0100

    temporary removed lax from samesite cookie

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 24 hours
-      sameSite: "lax",
+      sameSite: "none",
       httpOnly: true,
     },
     rolling: true,
