d15ba3a602c5d824ee965806009ab830f58f6789
Clément <poudree.clement@gmail.com>
2024-11-06 19:11:19 +0100

    Set samesite to false for testing purpose

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 24 hours
-      sameSite: "none",
+      sameSite: false,
       httpOnly: true,
       secure: true, // Set secure to true only in production
     },
