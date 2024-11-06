bbea2e34c527844d9da3ca1ea792759aa5641846
Clément <poudree.clement@gmail.com>
2024-11-06 11:36:56 +0100

    Set cookie samesite to lax

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 24 hours
-      sameSite: "none",
+      sameSite: "lax",
       secure: true,
       httpOnly: true,
     },
