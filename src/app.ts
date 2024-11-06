8e22a9685870f1d1273cb88ccc705eb048a9085f
Clément <poudree.clement@gmail.com>
2024-11-06 20:50:03 +0100

    removed samesite

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,6 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 24 hours
-      sameSite: "none",
       httpOnly: true,
       secure: true, // Set secure to true only in production
     },
